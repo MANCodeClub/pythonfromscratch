1da056c3b683de9bed9acab17ce103e3df26d76a
Nicolas Viel <le.nicolas.viel@gmail.com>
2020-04-11 08:34:25 +0200

    reprise des solutions du pb 002

--- musculation/002_brexit/solve.py
+++ musculation/002_brexit/solve.py
@@ -4,5 +4,14 @@ def affiche_drapeau(demi_taille):
     # FIXME: remplacer le code de cette fonction
     print("devrait afficher le drapeau à la bonne taille")
 
+
+"""Au démarrage du programme sys.argv est une liste contenant tous les paramètres transmis par la ligne de commande.
+argv[0] est le nom du script et on espère avoir le demi-coté dans argv[1]."""
+if len(sys.argv) != 2:
+    print("Indiquer le demi coté en paramètre:")
+    print("Par exemple:")
+    print("> python solve.py 5")
+    exit(1)
+
 demi_taille = int(sys.argv[1])
 affiche_drapeau(demi_taille)
